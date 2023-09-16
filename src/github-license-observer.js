00d2d0e30a5a28e293a3226901c06f50795efaec
Nicolas Martyanoff <nicolas@n16f.net>
2023-09-16 17:37:03 +0200

    fix detection of AGPL-3.0, GPL-3.0 and LGPL-3.0
    Infortunately the license list on the opensource.org website has a limited
    list of SPDX identifiers.

--- src/github-license-observer.js
+++ src/github-license-observer.js
@@ -3,29 +3,30 @@
 // It is almost certain that GitHub does not always use the SPDX license name
 // for the license field. We will have to test and adapt.
 const ossLicenses = [
-  "BSD-1-Clause", "AFL-3.0", "APL-1.0", "Apache-2.0", "Apache-1.1",
-  "APSL-2.0", "Artistic-1.0-Perl", "Artistic-1.0", "Artistic-2.0", "AAL",
-  "BSL-1.0", "BSD-2-Clause-Patent", "CECILL-2.1", "CERN-OHL-P-2.0",
-  "CERN-OHL-S-2.0", "CERN-OHL-W-2.0", "CDDL-1.0", "CPAL-1.0", "CPL-1.0",
-  "CATOSL-1.1", "CAL-1.0", "CUA-OPL-1.0", "EPL-1.0", "EPL-2.0", "eCos-2.0",
-  "ECL-1.0", "ECL-2.0", "EFL-1.0", "EFL-2.0", "Entessa", "EUDatagrid",
-  "EUPL-1.2", "Fair", "Frameworx-1.0", "AGPL-3.0-only", "GPL-2.0",
-  "GPL-3.0-only", "LGPL-2.1", "LGPL-3.0-only", "LGPL-2.0-only", "HPND",
-  "IPL-1.0", "Intel", "IPA", "ISC", "Jam", "LPPL-1.3c", "BSD-3-Clause-LBNL",
-  "LiLiQ-P-1.1", "LiLiQ-Rplus-1.1", "MirOS", "MIT-0", "Motosoto", "MPL-1.1",
-  "MPL-2.0", "MPL-1.0", "MulanPSL-2.0", "Multics", "NASA-1.3", "Naumen",
-  "NOKIA", "NPOSL-3.0", "NTP", "OGTSL", "OLFL-1.3", "OSL-2.1", "OSL-1.0",
-  "OLDAP-2.8", "OSET-PL-2.1", "PHP-3.0", "PHP-3.01", "PSF-2.0", "RPSL-1.0",
-  "RPL-1.5", "RPL-1.1", "OFL-1.1", "SimPL-2.0", "SISSL", "SPL-1.0",
-  "BSD-2-Clause", "BSD-3-Clause", "CNRI-Python", "EUPL-1.1", "MIT", "NGPL",
-  "OSL-3.0", "PostgreSQL", "QPL-1.0", "RSCPL", "Sleepycat", "Watcom-1.0",
-  "UPL-1.0", "NCSA", "Unlicense", "VSL-0.1", "W3C-20150513", "wxWindows",
-  "Xnet", "Zlib", "Unicode-DFS-2015", "UCL-1.0", "0BSD", "ZPL-2.0",
-  "ZPL-2.1"];
+  "0BSD", "AAL", "AFL-3.0", "AGPL-3.0", "AGPL-3.0-only", "APL-1.0",
+  "APSL-2.0", "Apache-1.1", "Apache-2.0", "Artistic-1.0", "Artistic-1.0-Perl",
+  "Artistic-2.0", "BSD-1-Clause", "BSD-2-Clause", "BSD-2-Clause-Patent",
+  "BSD-3-Clause", "BSD-3-Clause-LBNL", "BSL-1.0", "CAL-1.0", "CATOSL-1.1",
+  "CDDL-1.0", "CECILL-2.1", "CERN-OHL-P-2.0", "CERN-OHL-S-2.0",
+  "CERN-OHL-W-2.0", "CNRI-Python", "CPAL-1.0", "CPL-1.0", "CUA-OPL-1.0",
+  "ECL-1.0", "ECL-2.0", "EFL-1.0", "EFL-2.0", "EPL-1.0", "EPL-2.0",
+  "EUDatagrid", "EUPL-1.1", "EUPL-1.2", "Entessa", "Fair", "Frameworx-1.0",
+  "GPL-2.0", "GPL-3.0", "GPL-3.0-only", "HPND", "IPA", "IPL-1.0", "ISC",
+  "Intel", "Jam", "LGPL-2.0-only", "LGPL-2.1", "LGPL-3.0", "LGPL-3.0-only",
+  "LPPL-1.3c", "LiLiQ-P-1.1", "LiLiQ-Rplus-1.1", "MIT", "MIT-0", "MPL-1.0",
+  "MPL-1.1", "MPL-2.0", "MirOS", "Motosoto", "MulanPSL-2.0", "Multics",
+  "NASA-1.3", "NCSA", "NGPL", "NOKIA", "NPOSL-3.0", "NTP", "Naumen",
+  "OFL-1.1", "OGTSL", "OLDAP-2.8", "OLFL-1.3", "OSET-PL-2.1", "OSL-1.0",
+  "OSL-2.1", "OSL-3.0", "PHP-3.0", "PHP-3.01", "PSF-2.0", "PostgreSQL",
+  "QPL-1.0", "RPL-1.1", "RPL-1.5", "RPSL-1.0", "RSCPL", "SISSL", "SPL-1.0",
+  "SimPL-2.0", "Sleepycat", "UCL-1.0", "UPL-1.0", "Unicode-DFS-2015",
+  "Unlicense", "VSL-0.1", "W3C-20150513", "Watcom-1.0", "Xnet", "ZPL-2.0",
+  "ZPL-2.1", "Zlib", "eCos-2.0", "wxWindows",
+];
 
 // Built using regexp-opt in Emacs and some manual adjustments since Emacs
 // regexps and JS regexps are different.
-const ossLicenseREString = "(?:0BSD|A(?:AL|FL-3.0|GPL-3.0-only|P(?:(?:L-1|SL-2).0)|pache-(?:1.1|2.0)|rtistic-(?:1.0(?:-Perl)?|2.0))|BS(?:D-(?:1-Clause|2-Clause(?:-Patent)?|3-Clause(?:-LBNL)?)|L-1.0)|C(?:A(?:L-1.0|TOSL-1.1)|DDL-1.0|E(?:CILL-2.1|RN-OHL-(?:[PSW]-2.0))|NRI-Python|(?:PA?|UA-OP)L-1.0)|E(?:CL-(?:[12].0)|FL-(?:[12].0)|PL-(?:[12].0)|U(?:Datagrid|PL-1.[12])|ntessa)|F(?:air|rameworx-1.0)|GPL-(?:2.0|3.0-only)|HPND|I(?:P(?:A|L-1.0)|SC|ntel)|Jam|L(?:GPL-(?:2.(?:0-only|1)|3.0-only)|PPL-1.3c|iLiQ-(?:(?:P|Rplus)-1.1))|M(?:IT(?:-0)?|PL-(?:1.[01]|2.0)|irOS|otosoto|ul(?:anPSL-2.0|tics))|N(?:ASA-1.3|CSA|GPL|OKIA|POSL-3.0|TP|aumen)|O(?:FL-1.1|GTSL|L(?:DAP-2.8|FL-1.3)|S(?:ET-PL-2.1|L-(?:1.0|2.1|3.0)))|P(?:HP-3.01?|SF-2.0|ostgreSQL)|QPL-1.0|R(?:P(?:L-1.[15]|SL-1.0)|SCPL)|S(?:ISSL|PL-1.0|imPL-2.0|leepycat)|U(?:CL-1.0|PL-1.0|n(?:icode-DFS-2015|license))|VSL-0.1|W(?:3C-20150513|atcom-1.0)|Xnet|Z(?:PL-2.[01]|lib)|eCos-2.0|wxWindows)";
+const ossLicenseREString = "(?:0BSD|A(?:AL|FL-3.0|GPL-3.0(?:-only)?|P(?:(?:L-1|SL-2).0)|pache-(?:1.1|2.0)|rtistic-(?:1.0(?:-Perl)?|2.0))|BS(?:D-(?:1-Clause|2-Clause(?:-Patent)?|3-Clause(?:-LBNL)?)|L-1.0)|C(?:A(?:L-1.0|TOSL-1.1)|DDL-1.0|E(?:CILL-2.1|RN-OHL-(?:[PSW]-2.0))|NRI-Python|(?:PA?|UA-OP)L-1.0)|E(?:CL-(?:[12].0)|FL-(?:[12].0)|PL-(?:[12].0)|U(?:Datagrid|PL-1.[12])|ntessa)|F(?:air|rameworx-1.0)|GPL-(?:2.0|3.0(?:-only)?)|HPND|I(?:P(?:A|L-1.0)|SC|ntel)|Jam|L(?:GPL-(?:2.(?:0-only|1)|3.0(?:-only)?)|PPL-1.3c|iLiQ-(?:(?:P|Rplus)-1.1))|M(?:IT(?:-0)?|PL-(?:1.[01]|2.0)|irOS|otosoto|ul(?:anPSL-2.0|tics))|N(?:ASA-1.3|CSA|GPL|OKIA|POSL-3.0|TP|aumen)|O(?:FL-1.1|GTSL|L(?:DAP-2.8|FL-1.3)|S(?:ET-PL-2.1|L-(?:1.0|2.1|3.0)))|P(?:HP-3.01?|SF-2.0|ostgreSQL)|QPL-1.0|R(?:P(?:L-1.[15]|SL-1.0)|SCPL)|S(?:ISSL|PL-1.0|imPL-2.0|leepycat)|U(?:CL-1.0|PL-1.0|n(?:icode-DFS-2015|license))|VSL-0.1|W(?:3C-20150513|atcom-1.0)|Xnet|Z(?:PL-2.[01]|lib)|eCos-2.0|wxWindows)";
 
 const ossLicenseRE = new RegExp(ossLicenseREString, "i");
 const ossLicenseREAnchored = new RegExp("^" + ossLicenseREString + "$", "i");
